55168053e6aa9643c5e2328c14cf0b54f83e3f81
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-09-13 12:44:41 -0500

    promblem 4 complete

--- loopLabs2.js
+++ loopLabs2.js
@@ -8,7 +8,7 @@ for (i = 50; i >= 20; i--) {
 
 // 2.
 
-for (i =2; i >=1; i++) {
+for (i = 2; i >=1; i++) {
 	console.log(i);
 }
 
@@ -16,6 +16,19 @@ for (i =2; i >=1; i++) {
 
 const classmates = ["Bridjet", "Nathan", "Matias", "Mike", "Jason", "Reuben", "Ben", "Lane", "John", "Chris", "Terry", "Ann", "Katelyn", "Naomi", "Jeff"];
 
-for (i =0; i <= classmates.length; i += 2) {
+for (i = 0; i <= classmates.length; i += 2) {
 	console.log(classmates[i])
 }
+
+// 4.
+
+const menu = [{name: "pizza", glutenFree: false},{name: "salad", glutenFree: true},{name: "donut", glutenFree: false},{name: "steak", glutenFree: true},{name: "chicken", glutenFree: true},{name: "cheeseburger", glutenFree: false}];
+const gfFreeMenu = []
+
+for (let i = 0; i < menu.length; i++) {
+	if (menu[i].glutenFree === true) {
+		gfFreeMenu.push(menu[i]);
+	 };
+};
+
+console.log(gfFreeMenu)
