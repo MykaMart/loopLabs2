c2721ca4d2f4fcedf2cae76d1b24d3ae4a0928d5
Myka Martinez <michaelmartinez-mann@Yin.local>
2017-09-13 12:12:07 -0500

    promblem 3 complete

--- loopLabs2.js
+++ loopLabs2.js
@@ -1,5 +1,21 @@
+// 1.
+
 for (i = 50; i >= 20; i--) {
 	if (i%3 === 0) {
 		console.log(i);
 	};
 };
+
+// 2.
+
+for (i =2; i >=1; i++) {
+	console.log(i);
+}
+
+// 3.
+
+const classmates = ["Bridjet", "Nathan", "Matias", "Mike", "Jason", "Reuben", "Ben", "Lane", "John", "Chris", "Terry", "Ann", "Katelyn", "Naomi", "Jeff"];
+
+for (i =0; i <= classmates.length; i += 2) {
+	console.log(classmates[i])
+}
